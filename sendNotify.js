09161f0106c53cf79a550758d1dba5614fce4266
Sky 夜空 <72479664+SkyNightovo@users.noreply.github.com>
2022-09-08 19:44:41 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -183,7 +183,7 @@ async function sendNotify(
   text,
   desp,
   params = {},
-  author = `\n\n本通知 By：https://github.com/leafTheFish/DeathNote\n通知时间：${new Date()}`,
+  author = `\n\n本通知 By：https://github.com/SkyNightovo/js\n通知时间：${new Date()}`,
 ) {
   //提供6种通知
   desp += author; //增加作者信息，防止被贩卖等
